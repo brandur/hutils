62478988269c1a35908c0ed4bb6d45f2f6e419b9
Brandur <brandur@mutelight.org>
2014-08-25 08:09:35 -0700

    Add `lfmt`

--- lib/hutils/text_visualizer.rb
+++ lib/hutils/text_visualizer.rb
@@ -2,22 +2,52 @@ require "term/ansicolor"
 
 module Hutils
   class TextVisualizer
-    extend Term::ANSIColor
-    include Term::ANSIColor
+    class LineVisualizer
+      include Term::ANSIColor
 
-    def self.display_message(message, colors:, compact:, highlights:, out:, indent: "")
-      message.to_a.sort_by { |k, v| k }.map { |k, v|
-        pair_to_string(k, v, colors: colors, highlights: highlights)
-      }.each_with_index { |display, i|
-        if compact
-          out.print(indent) if i == 0
-          out.print("#{display} ")
+      def initialize(colors:, compact:, highlights:, out:)
+        @colors = colors
+        @compact = compact
+        @highlights = highlights
+        @out = out
+      end
+
+      def display(message, indent: "")
+        message.to_a.sort_by { |k, v| k }.map { |k, v|
+          pair_to_string(k, v)
+        }.each_with_index { |display, i|
+          if @compact
+            @out.print(indent) if i == 0
+            @out.print("#{display} ")
+          else
+            marker = i == 0 ? "+ " : "  "
+            @out.puts "#{indent}#{marker}#{display}"
+          end
+        }
+        @out.puts ""
+      end
+
+      private
+
+      def colorize(method, str)
+        if @colors
+          send(method, str)
         else
-          marker = i == 0 ? "+ " : "  "
-          out.puts "#{indent}#{marker}#{display}"
+          str
         end
-      }
-      out.puts ""
+      end
+
+      def pair_to_string(k, v)
+        if v == true
+          colorize(:green, k)
+        else
+          if @highlights.include?(k)
+            colorize(:on_yellow, colorize(:black, "#{k}: #{v}"))
+          else
+            "#{colorize(:green, k)}: #{v}"
+          end
+        end
+      end
     end
 
     def initialize(colors:, compact:, highlights:, root:, out:)
@@ -26,6 +56,13 @@ module Hutils
       @highlights = highlights
       @out = out
       @root = root
+
+      @line_visualizer = LineVisualizer.new(
+        colors: colors,
+        compact: compact,
+        highlights: highlights,
+        out: out
+      )
     end
 
     def display
@@ -34,36 +71,11 @@ module Hutils
 
     private
 
-    def self.colorize(method, str, colors:)
-      if colors
-        send(method, str)
-      else
-        str
-      end
-    end
-
-    def self.pair_to_string(k, v, colors:, highlights:)
-      if v == true
-        colorize(:green, k, colors: colors)
-      else
-        if highlights.include?(k)
-          colorize(:on_yellow, colorize(:black, "#{k}: #{v}", colors: colors), colors: colors)
-        else
-          "#{colorize(:green, k, colors: colors)}: #{v}"
-        end
-      end
-    end
-
     def display_node(node)
       if !node.common.empty?
         # the "- 1" is because the root node is empty
         indent = "\t" * (node.depth - 1)
-        TextVisualizer.display_message(node.common,
-          colors: @colors,
-          compact: @compact,
-          highlights: @highlights,
-          indent: indent,
-          out: @out)
+        @line_visualizer.display(node.common, indent: indent)
       end
       node.slots.each { |slot| display_node(slot) }
     end
