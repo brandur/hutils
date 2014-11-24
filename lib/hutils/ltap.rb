c8a720c4eb1a583670739c4e25f4183715a95407
Brandur <brandur@mutelight.org>
2014-11-23 22:50:46 -0800

    Add lvat support
    Support for [lvat](https://github.com/brandur/lvat) (fast trace lookup).

--- lib/hutils/ltap.rb
+++ lib/hutils/ltap.rb
@@ -1,4 +1,5 @@
 require_relative "ltap/conf"
+require_relative "ltap/lvat_drainer"
 require_relative "ltap/paper_trail_drainer"
 require_relative "ltap/splunk_drainer"
 require_relative "ltap/time_bound_parser"
